9439e5526cab49e5ec134ace9b1863e0a4e42837
riyagondhiya <riyagondhiya@gmail.com>
2025-07-15 13:11:25 +0530

    node

---
+++ even_odd.js
@@ -0,0 +1,11 @@
+const n=7
+
+console.log(" find if number is even or odd")
+if(n%2)
+{
+    console.log("odd number")
+}
+else
+{
+    console.log("even number")
+}
---
+++ leap.js
@@ -0,0 +1,10 @@
+const a=5
+const b=9
+if(a<b)
+{
+    console.log("b is bigger")
+}
+else
+{
+    console.log("a is bigger")
+}
