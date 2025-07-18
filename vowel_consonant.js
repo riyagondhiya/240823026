b1ae2b51db8d32f31485b8f25cea7cde13b2efb2
riyagondhiya <riyagondhiya@gmail.com>
2025-07-18 10:59:08 +0530

    node

---
+++ vowel_consonant.js
@@ -0,0 +1,7 @@
+let s = 'a';
+let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+if (vowels.includes(s)) {
+    console.log(`${s} is a vowel`);
+} else {
+    console.log(`${s} is a consonant`);
+}
