19c75b6da32029a8c21ba0e564d3af5211c00cd0
riyagondhiya <riyagondhiya@gmail.com>
2025-07-25 10:41:51 +0530

    node

---
+++ third_party_library/index.js
@@ -0,0 +1,3 @@
+const add=require("./add.js");
+console.log(add(11,22));
+console.log(add(19,12));
---
+++ third_party_library/path_fs_moment.js
@@ -0,0 +1,6 @@
+let path=require("path")
+let moment=require("moment")
+let fs=require("fs")
+
+console.log(path.dirname(__filename));
+console.log(path.extname("myfile.tst"));
