9674b0bd9ca2223d6e4b7e5520d1c2c6e4a2c236
riyagondhiya <riyagondhiya@gmail.coom>
2025-08-05 12:29:23 +0530

    node

--- third_party_library/index.js
+++ third_party_library/index.js
@@ -1,3 +1,16 @@
-const add=require("./add.js");
-console.log(add(11,22));
-console.log(add(19,12));
+// const add=require("./add.js");
+// console.log(add(11,22));
+// console.log(add(19,12));
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+n = len(numbers)
+
+print("Original list:", numbers)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if numbers[j] > numbers[j + 1]:
+            # Swap elements
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+print("Sorted list:", numbers)
